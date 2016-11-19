1f406caba6fd881453e7e0a0cf994a6c4017a960
brnbrns <brianburns48@gmail.com>
2016-11-19 00:37:30 -0600

    Updated programs

--- README.md
+++ README.md
@@ -14,3 +14,10 @@ Solves a system of differential equations of the form:
 x' = ax + by
 y' = cx + dy
 ```
+system.py utilizes the sympy library, which can be easily installed using pip.
+To install sympy so that system.py will run, run the following commands from a terminal or command line:
+```
+pip install mpmath
+pip install sympy
+```
+system.py will then be interpreted correctly.
--- secondOrder.py
+++ secondOrder.py
@@ -1,5 +1,6 @@
 ###############
 # Brian Burns
+#  MATH 238
 # ay'' + by' + cy = 0
 # secondOrder.py
 ###############
@@ -8,28 +9,28 @@ import sys
 import math
 
 def main():
+    # make sure we have enough arguments
     if len(sys.argv) != 4:
-        print("Usage: \"python3 secondOrder.py [a] [b] [c]\"")
+        print("Usage: \"python secondOrder.py [a] [b] [c]\"")
         sys.exit(0)
 
+    # grab the arguments
     a = float(sys.argv[1])
     b = float(sys.argv[2])
     c = float(sys.argv[3])
 
+    print("The solution to the equation is:\n")
+
+    # get the auxiliary roots
     root = pow(b, 2) - (4*a*c)
 
-    # if a == 0:
-    #     if b == 0:
-    #         print("y = 0")
-    #     else:
-    #         r = (-1*c) / b
-    #         print("y = c1e^("+str(r)+"t) + c2te^("+str(r)+"t)")
-
+    # y = c_1e^(r_1t) + c_2e^(r_2t)
     if root > 0:
         r1 = (-1*b + math.sqrt(root)) / 2*a
         r2 = (-1*b - math.sqrt(root)) / 2*a
         print("y = c_1e^("+str(r1)+"t) + c_2e^("+str(r2)+"t)")
 
+    # y = c_1e^(rt) + c_2te^(rt)
     elif root == 0:
         r = (-1*b) / (2*a)
         print("y = c_1e^("+str(r)+"t) + c_2te^("+str(r)+"t)")
@@ -37,8 +38,10 @@ def main():
     else:
         alpha = (-1*b) / (2*a)
         beta = math.sqrt((-1*root)) / (2*a)
+        # y = c_1cos(Bt) + c_2sin(Bt)
         if alpha == 0.0:
             print("y = c_1cos("+str(beta)+"t) + c_2sin("+str(beta)+"t)")
+        # y = c_1e^(at)cos(Bt) + c_2e^(at)sin(Bt)
         else:
             print("y = c_1e^("+str(alpha)+"t)cos("+str(beta)+"t) + c_2e^("+str(alpha)+"t)sin("+str(beta)+"t)")
 
